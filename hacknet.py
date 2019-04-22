7942722e78c1c64c89b4c5e710adc2097204bdcf
reffyMelon <46713296+reffyMelon@users.noreply.github.com>
2019-04-22 17:54:14 +0600

    hacknet.py

---
+++ hacknet.py
@@ -0,0 +1,32 @@
+#modules
+import socket
+import os
+import subprocess
+import sys
+import time
+from termcolor import colored
+#banner
+print(colored(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗', 'green')
+print(colored(' ██║  ██║██╔══██╗██╔════╝██║ ██╔╝     ████╗  ██║██╔════╝╚══██╔══╝', 'green')
+print(colored(' ███████║███████║██║     █████╔╝█████╗██╔██╗ ██║█████╗     ██║'   , 'green')
+print(colored(' ██╔══██║██╔══██║██║     ██╔═██╗╚════╝██║╚██╗██║██╔══╝     ██║'   , 'green')
+print(colored(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║'	 , 'green')
+print(colored(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝'	 , 'green')
+#main-menu
+def main():
+	print("H", end = "")
+	time.sleep(0.1)
+	print("e", end = "")
+	time.sleep(0.4)
+	print("l", end = "")
+	time.sleep(0.7)
+	print("l", end = "")
+	time.sleep(0.10)
+	print("o", end = "")
+	time.sleep(0.11)
+	print('Thank you for using my script')
+	print(' ______________________________________________ ')
+	print('|Author:Reffy|My VK:https://vk.com/mrgurutopyt |')
+	print('|My GitHub:https://github.com/reffyMelon       |')
+	print('|The creation date of the script:22.04.2019    |')
+	print('|______________________________________________|')
