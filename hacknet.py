4a099088a5dd71f49b139f14ef534f10e06f0942
reffyMelon <46713296+reffyMelon@users.noreply.github.com>
2019-04-22 20:10:47 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -38,7 +38,14 @@ def main():
 
 #Device information	
 	print('Name of your OS...')
-	print('os.name')
 	os.system('uname -o')
-	print('Check your MAC-address:')
+	print('Other system info...')
+	os.system('pkg update')
+	os.system('pkg upgrade')
+	os.system('pkg install neofetch')
+	os.system('neofetch')
+	if os.system("neofetch") == 1:
+		os.system('pkg install neofetch')
+	else:
+		os.system("neofetch")
 main()
