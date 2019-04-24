51acab32f86566cd508fc5c3b7679d1c755f32ed
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-04-25 00:03:58 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -1,40 +1,43 @@
+# -*- coding: utf-8 -*-
 #modules
 import socket
 import os
 import subprocess
 import sys
 import time
-#banner
+def banner():
 print(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗' )
 print(' ██║  ██║██╔══██╗██╔════╝██║ ██╔╝     ████╗  ██║██╔════╝╚══██╔══╝')
 print(' ███████║███████║██║     █████╔╝█████╗██╔██╗ ██║█████╗     ██║'   )
 print(' ██╔══██║██╔══██║██║     ██╔═██╗╚════╝██║╚██╗██║██╔══╝     ██║'   )
 print(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║'	 )
 print(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝'	 )
+print("""
+  ______________________________________________ 
+ |Author:Reffy|My VK:https://vk.com/mrgurutopyt |
+ |My GitHub:https://github.com/reffyMelon       |
+ |The creation date of the script:22.04.2019    |
+  ______________________________________________
+  ########################
+  # Developed for Termux #
+  ########################
+""")  
+
 #main-menu
 def main():
 #a lot of print's :)
-	print("|Thank", end = "")
-	time.sleep(0.1)
-	print("you", end = "")
-	time.sleep(0.4)
-	print("for", end = "")
-	time.sleep(0.7)
-	print("using", end = "")
-	time.sleep(0.10)
-	print("my", end = "")
-	time.sleep(0.11)
-	print('script')
-	print(' ______________________________________________ ')
-	print('|Author:Reffy|My VK:https://vk.com/mrgurutopyt |')
-	print('|My GitHub:https://github.com/reffyMelon       |')
-	print('|The creation date of the script:22.04.2019    |')
-	print('|______________________________________________|')
-	print("""
-    ____________________
-  +|Developed for Termux|+
- ==========================
-    """)
+banner()
+print("|Thank", end = "")
+time.sleep(0.1)
+print("you", end = "")
+time.sleep(0.4)
+print("for", end = "")
+time.sleep(0.7)
+print("using", end = "")
+time.sleep(0.10)
+print("my", end = "")
+time.sleep(0.11)
+print('script')
 #Device information	
 print('Name of your OS...')
 os.system('uname -o')
@@ -46,7 +49,52 @@ print('$##########################$')
 print('1)Gratitudes')
 print('2)Nmap')
 print('3)Metasploit')
-print('Port scan')
-print('Whois')
-print('IP Lookup')
+print('4)Port scan')
+print('5)Whois')
+print('6)IP Lookup')
+print('7)Quit')
+hpt = raw_input("[$hpt]>")
+def restart():
+	#copied from https://github.com/Gameye98/Lazymux
+	python = sys.executable
+	os.execl(python, python, * sys.argv)
+	curdir = os.getcwd()
+def back():
+	##copied from https://github.com/Gameye98/Lazymux
+	print('Quit!')
+	back = raw_input("[$hpt]>")
+	if back == "7":
+		restart()
+	else:
+		print('ERROR!')
+		time.sleep(2)
+		restart()
+		
+if hpt == "1":
+	print("""
+								       
+		############################################## =========
+		#Second developer:https://github.com/inkviz96# Writes port-scanner         
+		############################################## =========
+	""")
+Nmap = raw_input("[$hpt]>")
+if hpt == "2":
+	print('install Nmap')
+	subprocess.run("apt update && apt upgarde")
+	subprocess.run("pkg install nmap")
+	print("The installation is finished")
+Metasploit = raw_input("[$hpt]>")
+if hpt == "3":
+	print('install Metasploit')
+	subprocess.run("apt update && apt upgarde")
+	subprocess.run("pkg install unstable-repo")
+	subprocess.run("pkg install metasploit")	
+Quit = raw_input("[$hpt]>")
+if hpt == "7":
+	print("Quit back to menu")
+	back()
+	else:
+		print('ERROR!')
+		time.sleep(2)
+		restart()
 main()
