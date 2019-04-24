2e6dc00bf55570f09586a64bf325f25254e7bc46
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-04-24 23:01:10 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -14,23 +14,23 @@ print(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝ 
 #main-menu
 def main():
 #a lot of print's :)
-print("|Thank", end = "")
-time.sleep(0.1)
-print("you", end = "")
-time.sleep(0.4)
-print("for", end = "")
-time.sleep(0.7)
-print("using", end = "")
-time.sleep(0.10)
-print("my", end = "")
-time.sleep(0.11)
-print('script')
-print(' ______________________________________________ ')
-print('|Author:Reffy|My VK:https://vk.com/mrgurutopyt |')
-print('|My GitHub:https://github.com/reffyMelon       |')
-print('|The creation date of the script:22.04.2019    |')
-print('|______________________________________________|')
-print("""
+	print("|Thank", end = "")
+	time.sleep(0.1)
+	print("you", end = "")
+	time.sleep(0.4)
+	print("for", end = "")
+	time.sleep(0.7)
+	print("using", end = "")
+	time.sleep(0.10)
+	print("my", end = "")
+	time.sleep(0.11)
+	print('script')
+	print(' ______________________________________________ ')
+	print('|Author:Reffy|My VK:https://vk.com/mrgurutopyt |')
+	print('|My GitHub:https://github.com/reffyMelon       |')
+	print('|The creation date of the script:22.04.2019    |')
+	print('|______________________________________________|')
+	print("""
     ____________________
   +|Developed for Termux|+
  ==========================
