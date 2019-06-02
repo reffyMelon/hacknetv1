fb1e1e33da4f81939ab2ce28b93466b143c99109
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-02 23:17:08 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -4,38 +4,45 @@ import os # Use
 import subprocess # Use
 import sys # Use
 import time # Use
-from socket import socket, gethostbyname, AF_INET, SOCK_STREAM #use
-try:
-	from prettytable import PrettyTable
-except:
-	print("Install prettytable")
-	os.system("pip install prettytable")
 try:
 	import colorama
 except:
 	print("Install colorama")
 	os.system("pip install colorama")
+
 try:
 	from termcolor import colored
 except:
 		print("Install termcolor")
 		os.system("pip install termcolor")
-from colorama import *
-try:
-	import progressbar
-except:
-	print("Install progressbar")
-	os.system("pip install progressba")
-
-
 
 os.system("clear")
+"""
+Tool:
+The tool is created exclusively for the program Thermex on Android.
+----------------------------------------------------------------------------------------
+Packages are installed via pkg.
+----------------------------------------------------------------------------------------
+Packages will be configured depending on what button you press.
+If you have root permissions you are allowed more. 
+The package will be updated within a month.
+----------------------------------------------------------------------------------------
+Tools that can be installed through this package:
+----------------------------------------------------------------------------------------
+Metasploit Framework,
+Nmap,
+Neotech,
+Check open ports. //This can also be done via nmap in more detail.
+----------------------------------------------------------------------------------------
+Thank you for using our package to install other utilities on Thermex.
+"""
+
 def banner():
-	print(colored(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗', "blue"))
+	print(colored(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗', "red"))
 	print(colored(' ██║  ██║██╔══██╗██╔════╝██║ ██╔╝     ████╗  ██║██╔════╝╚══██╔══╝', "blue"))
-	print(colored(' ███████║███████║██║     █████╔╝█████╗██╔██╗ ██║█████╗     ██║', "blue"))
+	print(colored(' ███████║███████║██║     █████╔╝█████╗██╔██╗ ██║█████╗     ██║', "red"))
 	print(colored(' ██╔══██║██╔══██║██║     ██╔═██╗╚════╝██║╚██╗██║██╔══╝     ██║', "blue"))
-	print(colored(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║', "blue"))
+	print(colored(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║', "red"))
 	print(colored(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝', "blue"))
 	#print("User:", str(os.environ.get("USERNAME"), "loading conf.")
 	print(colored("""
@@ -56,27 +63,57 @@ def main():
 		print('Name of your OS...')
 		os.system('uname -o') # Os command line
 		print(colored('Other system info...', "red"))
-		os.system("pkg install neofetch")
-		print(colored('Menu:', "green"))
+		os.system("neofetch")
+		print(colored('Menu:', "blue"))
 		print('$##########################$')
-		print(colored('1)Port scaner:', "green"))
-		#print(colored('3)Metasploit', "green"))
-		#print(colored('4)Port scan', "green"))
-		#print(colored('5)Whois', "green"))
-		#print(colored('6)IP Lookup', "green"))
-		print(colored('7)Quit', "green"))
+		print(colored('1)Gratitudes', "yellow"))
+		print(colored('2)Nmap', "red"))
+		print(colored('3)Metasploit', "red"))
+		print(colored('4)Port scan', "yellow"))
+		print(colored('5)Whois', "green"))
+		print(colored('6)IP Lookup', "red"))
+		print(colored('7)Quit', "blue"))
 		print('----------------------------')
 		join()
+
+
 def menu():
 	os.system("clear")
 	main()
 def join():
 	while True:
-		hpt = input("[$HPT]>")
-		#так же можно сделать так:
-		#temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
-		#hpt = input(temp)
-		#functionality:
-	if hpt == "1":
-		print("Port scaner")
+		temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
+		hpt = input(temp)
+
+
+		if hpt == "1":
+				print("""
+													
+				############################################## =========
+				#Second developer:https://github.com/inkviz96# Writes port-scanner         
+				############################################## =========
+				""")
+
+
+		if hpt == "2":
+			print('install Nmap') #print
+			os.system("apt update && apt upgrade") #Update apt and System termux
+			os.system("pkg install nmap") #Install nmap
+			print("The installation is finished")
+
+
+		if hpt == "3":
+			print('install Metasploit')
+			os.system("apt update && apt upgrade") #Update apt and System termux
+			os.system("pkg install unstable-repo") #Install repositories
+			os.system("pkg install metasploit") #Install metasploit
+
+
+		if hpt == "7":
+			print("Goodbay")
+			exit()
+
+
+
+#Start old process for programm
 menu()
