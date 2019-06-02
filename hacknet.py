c1736e4c212ae9b22f009e20f7432ec46a491235
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-02 23:24:15 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -17,25 +17,6 @@ except:
 		os.system("pip install termcolor")
 
 os.system("clear")
-"""
-Tool:
-The tool is created exclusively for the program Thermex on Android.
-----------------------------------------------------------------------------------------
-Packages are installed via pkg.
-----------------------------------------------------------------------------------------
-Packages will be configured depending on what button you press.
-If you have root permissions you are allowed more. 
-The package will be updated within a month.
-----------------------------------------------------------------------------------------
-Tools that can be installed through this package:
-----------------------------------------------------------------------------------------
-Metasploit Framework,
-Nmap,
-Neotech,
-Check open ports. //This can also be done via nmap in more detail.
-----------------------------------------------------------------------------------------
-Thank you for using our package to install other utilities on Thermex.
-"""
 
 def banner():
 	print(colored(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗', "red"))
@@ -82,9 +63,9 @@ def menu():
 	main()
 def join():
 	while True:
-		temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
-		hpt = input(temp)
-
+		#temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
+		#hpt = input(temp)
+		hpt = input("[$HPT]>")
 
 		if hpt == "1":
 				print("""
