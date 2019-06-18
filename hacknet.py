6094ce8e48af310864e50913a6d901236bcd5112
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-18 17:28:29 +0600

    Update hacknet.py

--- hacknet.py
+++ hacknet.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 #modules
 import os 
-import sys
-import time
-import colorama
-from colorama import *
-from termcolor import colored
-#banner
+import subprocess 
+import sys 
+import time 
+try:
+	import colorama
+except:
+	print("Install colorama")
+	os.system("pip install colorama")
+
+try:
+	from termcolor import colored
+except:
+		print("Install termcolor")
+		os.system("pip install termcolor")
+
 os.system("clear")
 def banner():
 	print(colored(' ██╗  ██╗ █████╗  ██████╗██╗  ██╗     ███╗   ██╗███████╗████████╗', "red"))
@@ -20,14 +29,13 @@ def banner():
 	print(colored('|		My GitHub:https://github.com/reffyMelon					  |', "red"))
 	print(colored('|		The creation date of the script:22.04.2019				  |', "red"))
 	print(colored(' ------------------------------------------------------------------', "red"))
-	#print("User:", str(os.environ.get("USERNAME"), "loading conf.")
 #main-menu
 def main():
 		#Device information	
 		banner()
-		print(colored('Name of your OS...', "blue"))
-		#можно сделать так:os.system('uname -o')
+		print('Name of your OS...')
 		print(sys.platform)
+		os.system('uname -o')
 		print(colored('Other system info...', "blue"))
 		if os.system("neofetch") == 1:
 			os.system('pkg install neofetch')
@@ -37,7 +45,7 @@ def main():
 		print(colored('3)Whois', "blue"))
 		print(colored('4)Information gathering', "blue"))
 		print(colored('5)Exploitation', "blue"))
-		print(colored('6)', "blue"))
+		print(colored('6)Exit', "blue"))
 		print(colored('$#############################$', "blue"))
 		join()
 def menu():
@@ -45,12 +53,14 @@ def menu():
 	main()
 def join():
 	while True:
-		hpt = input("[$HPT]>", "red")
-		#так же можно сделать так:
-		#temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
-		#hpt = input(temp)
-		#functionality:
-	if hpt == "1":
-		print(colored('Ports-Scanner', "blue"))
+		hpt = hpt = input("[$HPT]>", "blue")
+		#if hpt == "1":
 
+		#if hpt == "2":
+			
+		#if hpt == "3":
+		
+		if hpt == "7":
+			print("Bye!")
+			exit()
 menu()
