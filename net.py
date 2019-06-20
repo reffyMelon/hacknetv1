775f883cee84a0309ce70936e267be7d759bd784
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-20 14:18:56 +0600

    Update net.py

--- net.py
+++ net.py
@@ -40,8 +40,9 @@ def main():
 		print(colored('$#############################$', "blue"))
 		print(colored('1)Ports-Scanner', "blue"))
 		print(colored('3)Whois', "blue"))
-		print(colored('4)Tools installation', "blue"))
-		print(colored('5)Exit', "blue"))
+		print(colored('4)DNS Lookup', "blue"))
+		print(colored('5)Tools installation', "blue"))
+		print(colored('6)Exit', "blue"))
 		print(colored('$#############################$', "blue"))
 		join()
 def menu():
@@ -51,25 +52,15 @@ def join():
 	while True:
 		#temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
 		#hpt = input(temp)
-		hpt = hpt = input("[$HPT]>", "blue")
+		hpt = input("[$HPT]>", "blue")
 
 		if hpt == "1":
 			portscanner()
-		if hpt == "2":
-			print('install Nmap') #print
-			os.system("apt update && apt upgrade") #Update apt and System termux
-			os.system("pkg install nmap") #Install nmap
-			print("The installation is finished")
-
-
-		if hpt == "3":
-			print('install Metasploit')
-			os.system("apt update && apt upgrade") #Update apt and System termux
-			os.system("pkg install unstable-repo") #Install repositories
-			os.system("pkg install metasploit") #Install metasploit
-
-
+		#if hpt == "2":
+			
+		#if hpt == "3":
+			
 		if hpt == "7":
-			print("Goodbay")
+			print("Goodbye!")
 			exit()
 menu()
