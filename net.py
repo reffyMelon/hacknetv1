bbe98721a3656d6a0bdc0d9c3593c4e17df33538
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-21 18:16:46 +0600

    Update net.py

--- net.py
+++ net.py
@@ -24,9 +24,9 @@ def banner():
 	print(colored(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║', "red"))
 	print(colored(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝', "red"))
 	print(colored(' __________________________________________________________________', "red"))
-	print(colored('|		Author:Reffy|My VK:https://vk.com/mrgurutopyt			  |', "red"))
-	print(colored('|		My GitHub:https://github.com/reffyMelon					  |', "red"))
-	print(colored('|		The creation date of the script:22.04.2019				  |', "red"))
+	print(colored('|		Author:Reffy|My VK:https://vk.com/mrgurutopyt	  |', "red"))
+	print(colored('|		My GitHub:https://github.com/reffyMelon		  |', "red"))
+	print(colored('|		The creation date of the script:22.04.2019	  |', "red"))
 	print(colored(' ------------------------------------------------------------------', "red"))
 	#print("User:", str(os.environ.get("USERNAME"), "loading conf.")
 #main-menu
@@ -39,10 +39,10 @@ def main():
 		print(colored('Menu:', "blue"))
 		print(colored('$#############################$', "blue"))
 		print(colored('1)Ports-Scanner', "blue"))
-		print(colored('3)Whois', "blue"))
-		print(colored('4)DNS Lookup', "blue"))
-		print(colored('5)Tools installation', "blue"))
-		print(colored('6)Exit', "blue"))
+		print(colored('2)Whois', "blue"))
+		print(colored('3)DNS Lookup', "blue"))
+		print(colored('4)Tools installation', "blue"))
+		print(colored('5)Exit', "blue"))
 		print(colored('0)Restart', "blue"))
 		print(colored('$#############################$', "blue"))
 		join()
@@ -57,7 +57,8 @@ def join():
 
 		if hpt == "1":
 			portscanner()
-		#if hpt == "2":
+		if hpt == "2":
+			whois()
 			
 		#if hpt == "3":
 			
