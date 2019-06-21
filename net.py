3ed7092ba91ad70edc1ce7b37cc18bfc2519b2cf
Guruplayer <46713296+reffyMelon@users.noreply.github.com>
2019-06-21 18:09:53 +0600

    Update net.py

--- net.py
+++ net.py
@@ -24,13 +24,14 @@ def banner():
 	print(colored(' ██║  ██║██║  ██║╚██████╗██║  ██╗     ██║ ╚████║███████╗   ██║', "red"))
 	print(colored(' ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝     ╚═╝  ╚═══╝╚══════╝   ╚═╝', "red"))
 	print(colored(' __________________________________________________________________', "red"))
-	print(colored('|		Author:Reffy|My VK:https://vk.com/mrgurutopyt	  |', "red"))
-	print(colored('|		My GitHub:https://github.com/reffyMelon		  |', "red"))
-	print(colored('|		The creation date of the script:22.04.2019	  |', "red"))
+	print(colored('|		Author:Reffy|My VK:https://vk.com/mrgurutopyt			  |', "red"))
+	print(colored('|		My GitHub:https://github.com/reffyMelon					  |', "red"))
+	print(colored('|		The creation date of the script:22.04.2019				  |', "red"))
 	print(colored(' ------------------------------------------------------------------', "red"))
 	#print("User:", str(os.environ.get("USERNAME"), "loading conf.")
 #main-menu
-def main():	
+def main():
+		#Device information	
 		banner()
 		print(colored('Name of your OS...', "blue"))
 		os.system('uname -o')
@@ -52,7 +53,7 @@ def join():
 	while True:
 		#temp = colored("$", "blue") + colored(str(os.environ.get("USERNAME")), "red") + "||" + colored(str(os.getcwd()), "green") + colored(":-->", "yellow")
 		#hpt = input(temp)
-		hpt = input(colored("[$HPT]>", "blue"))
+		hpt = input("[$HPT]>", "blue")
 
 		if hpt == "1":
 			portscanner()
@@ -63,6 +64,7 @@ def join():
 		if hpt == "6":
 			print(colored("Goodbye!", "red"))
 			exit()
-		#if hpt == "0":
-			#reset()
+		if hpt == "0":
+			print(colored("Back to menu!", "red"))
+			main()
 menu()
